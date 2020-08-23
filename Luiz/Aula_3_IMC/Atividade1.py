6c47c918529b0d63046300b4f84b47db26e4c431
LuizNepomuceno <69309587+LuizNepomuceno@users.noreply.github.com>
2020-08-23 01:14:22 -0300

    AULA_3_Atividades

---
+++ Luiz/Aula_3_IMC/Atividade1.py
@@ -0,0 +1,27 @@
+#Índice de massa corporal (IMC):
+'''
+Requisitos:
+𝑖𝑚𝑐=𝑝𝑒𝑠𝑜/(𝑎𝑙𝑡𝑢𝑟𝑎^2 )
+Muito abaixo do peso  quando imc menor que 17
+Abaixo do peso normal quando imc entre 17 e 18.5
+Peso dentro do normal quando imc entre 18.5 e 25.0
+Acima do peso normal  quando imc entre 25 e 30
+Muito acima do peso quando imc acima de 30
+Somente mostrar true e false (sem estrutura de decisão)
+Criar e gerenciar projeto:
+Trabalhar em grupos com 4 no projeto, com todos fazendo o programa todo dessa vez (fluxo de trabalho conforme aula anterior (Fluxo de trabalho do Gerente de Integração)). Selecionar o gerente de projeto e o gerente de integração na equipe.
+Issues
+Milestones
+Documentar (wiki)
+'''
+
+#Atividade 3 IMC
+
+
+
+peso = 73
+altura = 1.70**2
+print(altura)
+
+print(peso / altura)
+
