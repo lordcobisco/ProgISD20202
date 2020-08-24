f33c906d82f5a1a46ecf56660d9332ed18778ccf
castrocarla <69760454+castrocarla@users.noreply.github.com>
2020-08-24 17:25:53 -0300

    Programa da aula 3
    Programa da aula 3

---
+++ Carla/Aula3/helloworld.py
@@ -0,0 +1,72 @@
+#Nada do que for colocado aqui aparecerá no programa]
+'''
+Nada do que for colocado aqui aparecerá no programa 
+'''
+
+print("Hello world")
+varString = "Carla Castro"
+print(varString)
+print(type(varString))
+
+varInt = 2
+print(varInt)
+print(type(varInt))
+
+varFloat = 2.2
+print(varFloat)
+print(type(varFloat))
+
+varBool = True
+print(varBool)
+print(type(varBool))
+
+import math
+print(math.sqrt(25))
+
+from math import sqrt
+print(sqrt(25))
+
+resultadosoma = 5+5
+print(resultadosoma)
+
+print(5+5) #soma
+
+print(2-5) #subtração
+
+print(2*5) #multiplicação
+
+print(10/2) #Divisão
+
+print(2**10) #Exponenciação
+
+print(10/9) #Parte inteira da divisão
+
+print(10%11) #Resto da divisão
+a, b, c = 2,4,8
+a, b, c = a*2, a+b+c, a*b*c
+(x, y) = (1,2)
+
+varLogica = True
+print("Valor de 'not varLogica':", not varLogica)
+
+varLogica1 = True
+varLogica2 = False
+print("Valor de' varLogica1 e varLogica2':", varLogica1 and varLogica2)
+
+print(2==2)
+print(2!=2)
+print(2>2)
+print(2<2)
+print(2>=2)
+print(2<=2)
+
+num = input("Digite um número")
+
+login = input("Digite o login")
+
+senha = input("senha")
+
+print("O usúario informado foi: %s , e a Senha informada foi: %s")
+
+
+
