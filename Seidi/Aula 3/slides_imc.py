6376dd07a82de6c5e2bd913707dd536fb5fcde59
Seidikun <seidi.yamauti@gmail.com>
2020-08-26 18:45:55 -0300

    adicionados exercícios dos slides e item l da atividade constextualizada 3

--- Seidi/Aula 3/slides_imc.py
+++ Seidi/Aula 3/slides_imc.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug 22 14:58:23 2020
+Created on Tue Aug 11 11:39:07 2020
 
 @author: seidi
 """
 
-peso    = float(input("Digite seu peso (em kg): "))
-altura  = float(input("Digite sua altura (em m): "))
+peso    = 64 # em kg
+altura  = 1.67 # em m
 imc     = peso/altura**2
 
 print('valor do imc: ', imc)
-print('muito abaixo do peso: ', imc <= 17)
+print('muito abaixo do peso', imc <= 17)
 print('abaixo do normal', imc > 17 and imc <= 18.5)
 print('dentro do normal', imc > 18.5 and imc <= 25)
-print('acime do normal', imc >25 and imc <= 30)
+print('acima do normal', imc >25 and imc <= 30)
 print('muito acima do normal', imc > 30)
