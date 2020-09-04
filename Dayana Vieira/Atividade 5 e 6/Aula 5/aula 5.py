ef0f4da523960b15f2b7a3680684a6864cddf51f
dayssv <dayana.vieira@edu.isd.org.br>
2020-09-03 23:22:18 -0300

    Resposta atividade 5 e 6
    Boa noite professor, segue as respostas das atividades das aulas 5 e 6.
    
    Discente: Dayana Siva dos Santos Vieira

---
+++ Dayana Vieira/Atividade 5 e 6/Aula 5/aula 5.py
@@ -0,0 +1,45 @@
+lista = []
+print(lista)
+
+lista = ["Ó carro", "peixe", 123, 111]
+print (lista)
+
+lista = ["Ó carro", "peixe", 123, 111]
+nova_lista = ["pedra", lista]
+print (nova_lista)
+
+lista = ["O carro", "peixe", 123, 111]
+nova_lista = ["pedra", lista
+print (lista [0])
+print (lista [2])
+print (nova_lista [1])
+print (nova_lista [1][2])
+
+
+lista = ["O carro", "peixe", 123, 111]
+print(lista [-1])
+print (lista [-2])
+print{lista [-3]
+print (lista [-4])
+
+lista=["O carro" "peixe", 123, 111]
+nova_lista = ["pedra", lista]
+print (lista+nova_lista)
+print (lista*3)
+
+lista = ["O carro" "peixe", 123, 111]
+print ("peixe" in lista)
+
+numeros = [14,55,67,89,88,10,21,5]
+print (min(numeros))
+print (max (numeros))
+print (sum(numero)
+
+livros = ["Java", "Sqlserver", "Delphi", "Pyton"]
+livros.append("android")
+print(livros)
+
+
+
+
+
