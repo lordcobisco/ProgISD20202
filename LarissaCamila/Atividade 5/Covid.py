3967c2e0957e056ff16b96e5580c416a8e1f2605
larissa870 <69308920+larissa870@users.noreply.github.com>
2020-09-04 00:40:16 -0300

    Atualização

---
+++ LarissaCamila/Atividade 5/Covid.py
@@ -0,0 +1,87 @@
+# LISTAS #
+
+estado = ["Acre (AC)","Amazonas (AM)","Bahia(BA)","Ceará(CE)", "Pará (PA)","Paraiba (PB)","Pernambuco (PE)","Piauí (PI)","Rio de Janeiro (RJ)","Rio grande do norte (RN)"]
+populaçãoestado = [881935, 4144597, 14873064, 9132078, 8602865, 4018127, 9557071, 3273227, 17264943, 3506853]
+
+obitosacumulados = [612, 3649, 5397, 8409, 105778, 125539, 77405, 131906, 61748, 55153]
+novosobitos = [3, 10, 53, 25, 31, 15, 19, 10, 38, 8]
+
+casosacumulados = [24647, 120296, 256727, 214953, 199556, 105778, 125539, 77405, 223631, 61748]
+novoscasos = [9, 236, 665, 496, 385, 117, 445, 140, 329, 99]
+
+acre = [estado[0], populaçãoestado[0], obitosacumulados[0], novosobitos[0], casosacumulados[0], novoscasos[0]]
+amazonas = [estado[1], populaçãoestado[1], obitosacumulados[1], novosobitos[1], casosacumulados[1], novoscasos[1]]
+bahia = [estado[2], populaçãoestado[2], obitosacumulados[2], novosobitos[2], casosacumulados[2], novoscasos[2]]
+ceara = [estado[3], populaçãoestado[3], obitosacumulados[3], novosobitos[3], casosacumulados[3], novoscasos[3]]
+para = [estado[4], populaçãoestado[4], obitosacumulados[4], novosobitos[4], casosacumulados[4], novoscasos[4]]
+paraiba = [estado[5], populaçãoestado[5], obitosacumulados[5], novosobitos[5], casosacumulados[5], novoscasos[5]]
+pernambuco = [estado[6], populaçãoestado[6], obitosacumulados[6], novosobitos[6], casosacumulados[6], novoscasos[6]]
+piaui = [estado[7], populaçãoestado[7], obitosacumulados[7], novosobitos[7], casosacumulados[7], novoscasos[7]]
+riodejaneiro = [estado[8], populaçãoestado[8], obitosacumulados[8], novosobitos[8], casosacumulados[8], novoscasos[8]]
+riograndedonorte = [estado[9], populaçãoestado[9], obitosacumulados[9], novosobitos[9], casosacumulados[9], novoscasos[9]]
+
+#Regiões de saúde
+
+regioes = ("Baixo Acre e Purus","Triangulo","Seabra","19 região Brejo Santo", "Tocantins","11ª região","Recife","Vale do rio Guaribas","Baia da Ilha Grande","1ª Região de Saúde")
+populaçãoregioes = [15256, 16041, 8739, 11737, 157698, 10234, 99990, 7084, 203785, 34276]
+
+obitosacumulados2 = [8, 13, 1, 2, 114, 2, 92, 0, 151, 17]
+novosobitos2 = [0, 0, 0, 0, 0, 0, 0, 0, 4, 0]
+
+casosacumulados2 = [428, 1209, 18, 67, 3045, 54, 770,35, 4811, 298]
+novoscasos2 = [1, 0, 665, 0, 28, 0, 1, 0, 118, 0]
+
+baixoacre = [regioes[0], populaçãoregioes[0], obitosacumulados2[0], novosobitos2[0], casosacumulados2[0], novoscasos2[0]]
+triangulo = [regioes[1], populaçãoregioes[1], obitosacumulados2[1], novosobitos2[1], casosacumulados2[1], novoscasos2[1]]
+seabra = [regioes[2], populaçãoregioes[2], obitosacumulados2[2], novosobitos2[2], casosacumulados2[2], novoscasos2[2]]
+brejosanto = [regioes[3], populaçãoregioes[3], obitosacumulados2[3], novosobitos2[3], casosacumulados2[3], novoscasos2[3]]
+tocantins = [regioes[4], populaçãoregioes[4], obitosacumulados2[4], novosobitos2[4], casosacumulados2[4], novoscasos2[4]]
+regiao11 = [regioes[5], populaçãoregioes[5], obitosacumulados2[5], novosobitos2[5], casosacumulados2[5], novoscasos2[5]]
+recife = [regioes[6], populaçãoregioes[6], obitosacumulados2[6], novosobitos2[6], casosacumulados2[6], novoscasos2[6]]
+valedorio = [regioes[7], populaçãoregioes[7], obitosacumulados2[7], novosobitos2[7], casosacumulados2[7], novoscasos2[7]]
+baiadailha = [regioes[8], populaçãoregioes[8], obitosacumulados2[8], novosobitos2[8], casosacumulados2[8], novoscasos2[8]]
+regiao1 = [regioes[9], populaçãoregioes[9], obitosacumulados2[9], novosobitos2[9], casosacumulados2[9], novoscasos2[9]]
+
+# TUPLAS #
+
+estadotup = ("Acre (AC)","Amazonas (AM)","Bahia(BA)","Ceará(CE)", "Pará (PA)","Paraiba (PB)","Pernambuco (PE)","Piauí (PI)","Rio de Janeiro (RJ)","Rio grande do norte (RN)")
+populaçãoestadotup = (881935, 4144597, 14873064, 9132078, 8602865, 4018127, 9557071, 3273227, 17264943, 3506853)
+
+obitosacumuladostup = (612, 3649, 5397, 8409, 105778, 125539, 77405, 131906, 61748, 55153)
+novosobitostup = (3, 10, 53, 25, 31, 15, 19, 10, 38, 8)
+
+casosacumuladostup = (24647, 120296, 256727, 214953, 199556, 105778, 125539, 77405, 223631, 61748)
+novoscasostup = (9, 236, 665, 496, 385, 117, 445, 140, 329, 99)
+
+acretup = (estadotup[0], populaçãoestadotup[0], obitosacumuladostup[0], novosobitostup[0], casosacumuladostup[0], novoscasostup[0])
+amazonastup = (estadotup[1], populaçãoestadotup[1], obitosacumuladostup[1], novosobitostup[1], casosacumuladostup[1], novoscasostup[1])
+bahiatup = (estadotup[2], populaçãoestadotup[2], obitosacumuladostup[2], novosobitostup[2], casosacumuladostup[2], novoscasostup[2])
+cearatup = (estadotup[3], populaçãoestadotup[3], obitosacumuladostup[3], novosobitostup[3], casosacumuladostup[3], novoscasostup[3])
+paratup = (estadotup[4], populaçãoestadotup[4], obitosacumuladostup[4], novosobitostup[4], casosacumuladostup[4], novoscasostup[4])
+paraibatup = (estadotup[5], populaçãoestadotup[5], obitosacumuladostup[5], novosobitostup[5], casosacumuladostup[5], novoscasostup[5])
+pernambucotup = (estadotup[6], populaçãoestadotup[6], obitosacumuladostup[6], novosobitostup[6], casosacumuladostup[6], novoscasostup[6])
+piauitup = (estadotup[7], populaçãoestadotup[7], obitosacumuladostup[7], novosobitostup[7], casosacumuladostup[7], novoscasostup[7])
+riodejaneirotup = (estadotup[8], populaçãoestadotup[8], obitosacumuladostup[8], novosobitostup[8], casosacumuladostup[8], novoscasostup[8])
+riograndedonortetup = (estadotup[9], populaçãoestadotup[9], obitosacumuladostup[9], novosobitostup[9], casosacumuladostup[9], novoscasostup[9])
+
+# B #
+print("\n Número de casos acumulados no RJ: ")
+
+print("O Rio de Janeiro (RJ) apresenta os seguintes dados referentes ao N de casos acumulados: ", casosacumulados[8])
+print("O Rio de Janeiro (RJ) apresenta os seguintes dados referentes ao N de casos acumulados: ", casosacumuladostup[8])
+
+# C #
+print("\n \n Óbitos acumulados por estado: ")
+
+print(estado[0], obitosacumulados[0])
+print(estado[1], obitosacumulados[1])
+print(estado[2], obitosacumulados[2])
+print(estado[3], obitosacumulados[3])
+print(estado[4], obitosacumulados[4])
+print(estado[5], obitosacumulados[5])
+print(estado[6], obitosacumulados[6])
+print(estado[7], obitosacumulados[7])
+print(estado[8], obitosacumulados[8])
+print(estado[9], obitosacumulados[9])
+
+# Não consegui realizar o restante da atividade
