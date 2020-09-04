52623115f74599ad7bba0ae55230170e0f008dbc
Luan Rizzetto <luan.rizzetto@edu.isd.org.br>
2020-09-03 22:59:32 -0300

    Create AtivContex_5_remix.py

---
+++ LuanRizzetto/Exercício Contextualizado 5/AtivContex_5_remix.py
@@ -0,0 +1,220 @@
+#Listas
+
+Estados = ['Norte', 'RO', 11, '30/8/2020', 36, 1777225, 54679, 183, 1129, 4,
+'Norte', 'AC', 12, '30/8/2020', 36, 881935, 24638, 37, 609, 1,
+'Norte', 'AM', 13, '30/8/2020', 36, 4144597, 120060, 201, 3639, 5,
+'Norte', 'RR' , 14, '30/8/2020', 36, 605761, 43470, 171, 587, 0,
+'Norte', 'PA', 15, '30/8/2020', 36, 8602865, 199171, 925, 6115, 6,
+'Norte', 'AP', 16, '30/8/2020', 36, 845731, 42892, 121, 660, 1,
+'Norte', 'TO', 17, '30/8/2020', 36, 1572866, 50373, 722, 664, 6,
+'Nordeste', 'MA', 21, '30/8/2020', 36, 7075181, 151212, 317, 3436, 12,
+'Nordeste', 'PI', 22, '30/8/2020', 36, 3273227, 77265, 349, 1815, 11,
+'Nordeste', 'CE', 23, '30/8/2020', 36, 9132078, 214457, 363, 8384, 2,
+'Nordeste', 'RN', 24, '30/8/2020', 36, 3506853, 61649, 142, 2248, 6,
+'Nordeste', 'PB', 25, '30/8/2020', 36, 4018127, 105661, 130, 2435, 15,
+'Nordeste', 'PE', 26, '30/8/2020', 36, 9557071, 125094, 943, 7574, 27,
+'Nordeste', 'AL', 27, '30/8/2020', 36, 3337357, 78637, 154, 1878, 8,
+'Nordeste', 'SE', 28, '30/8/2020', 36, 2298696, 72417, 137, 1844, 5,
+'Nordeste', 'BA', 29, '30/8/2020', 36, 14873064, 256062, 1272, 5344, 42,
+'Sudeste', 'MG', 31, '30/8/2020', 36, 21168791, 215050, 2485, 5326, 56,
+'Sudeste', 'ES', 32, '30/8/2020', 36, 4018650, 110351, 326, 3135, 6,
+'Sudeste', 'RJ', 33, '30/8/2020', 36, 17264943, 223302, 345, 16027, 11,
+'Sudeste', 'SP', 35, '30/8/2020', 36, 45919049, 803404, 1982, 29978, 34,
+'Sul', 'PR', 41, '30/8/2020', 36, 11433957, 130603, 1636, 3251, 17,
+'Sul', 'SC', 42, '30/8/2020', 36, 7164788, 146864, 460, 2235, 17,
+'Sul', 'RS', 43, '30/8/2020', 36, 11377239, 124482, 66, 3395, 10,
+'Centro-Oeste', 'MS', 50, '30/8/2020', 36, 2778986, 48522, 499, 851, 11,
+'Centro-Oeste', 'MT', 51, '30/8/2020', 36, 3484466, 90337, 375, 2725, 19,
+'Centro-Oeste', 'GO', 52, '30/8/2020', 36, 7018354, 130863, 547, 3075, 15,
+'Centro-Oeste', 'DF', 53, '30/8/2020', 36, 3015268, 160796, 1270, 2469, 19,
+'Norte', 'RO', 'Porto Velho', 11, 110020, 11004, 'MADEIRA-MAMORE', '30/8/2020', 36, 529544, 26744, 96, 642, 0, 1,
+'Norte'	'RR', 'Boa Vista', 14, 140010, 14001, 'CENTRO NORTE', '30/8/2020', 36, 399213, 32364, 145, 454, 0, 1,
+'Norte', 'AM', 'Manaus', 13, 130260, 13001, 'MANAUS,  ENTORNO E ALTO RIO NEGRO', '30/8/2020', 36, 2182763, 42398, 52, 2246, 2, 1,
+'Nordeste', 'CE', 'Fortaleza', 23, 230440, 23001, '1ª REGIAO FORTALEZA', '30/8/2020', 36, 2669342, 46689, 58, 3778, 1, 1,
+'Nordeste', 'PE', 'Recife', 26, 261160, 26010, 'RECIFE', '30/8/2020', 36, 1645727, 31770, 128, 2283, 1, 1,
+'Nordeste', 'BA', 'Salvador', 29, 292740, 29020, 'SALVADOR', '30/8/2020', 36, 2872347, 76373, 111, 2419, 4, 1,
+'Sudeste', 'MG', 'Belo Horizonte', 31, 310620, 31016, 'BELO HORIZONTE/ NOVA LIMA/ CAETE', '30/8/2020', 36, 2512070, 33092, 0, 978, 10, 1,
+'Sudeste', 'RJ', 'Rio de Janeiro', 33, 330455, 33005, 'METROPOLITANA I', '30/8/2020', 36, 6718903, 90345, 82, 9634, 6, 1,
+'Sudeste', 'SP', 'São Paulo', 35, 355030, 35016, 'SAO PAULO', '30/8/2020', 36, 12252023, 257778, 250, 11400, 2, 1,
+'Sul', 'PR', 'Curitiba', 41, 410690, 41002, '2ª RS METROPOLITANA', '30/8/2020', 36, 1933105, 28647, 252, 976, 8, 1,
+'Centro-Oeste', 'DF', 'Brasília', 53, 530010, 53001, 'DISTRITO FEDERAL', '30/8/2020', 36, 3015268, 160796, 1270, 2469, 19, 1]
+
+#Tuplas
+
+EstadosT = ('Norte', 'RO', 11, '30/8/2020', 36, 1777225, 54679, 183, 1129, 4,
+'Norte', 'AC', 12, '30/8/2020', 36, 881935, 24638, 37, 609, 1,
+'Norte', 'AM', 13, '30/8/2020', 36, 4144597, 120060, 201, 3639, 5,
+'Norte', 'RR' , 14, '30/8/2020', 36, 605761, 43470, 171, 587, 0,
+'Norte', 'PA', 15, '30/8/2020', 36, 8602865, 199171, 925, 6115, 6,
+'Norte', 'AP', 16, '30/8/2020', 36, 845731, 42892, 121, 660, 1,
+'Norte', 'TO', 17, '30/8/2020', 36, 1572866, 50373, 722, 664, 6,
+'Nordeste', 'MA', 21, '30/8/2020', 36, 7075181, 151212, 317, 3436, 12,
+'Nordeste', 'PI', 22, '30/8/2020', 36, 3273227, 77265, 349, 1815, 11,
+'Nordeste', 'CE', 23, '30/8/2020', 36, 9132078, 214457, 363, 8384, 2,
+'Nordeste', 'RN', 24, '30/8/2020', 36, 3506853, 61649, 142, 2248, 6,
+'Nordeste', 'PB', 25, '30/8/2020', 36, 4018127, 105661, 130, 2435, 15,
+'Nordeste', 'PE', 26, '30/8/2020', 36, 9557071, 125094, 943, 7574, 27,
+'Nordeste', 'AL', 27, '30/8/2020', 36, 3337357, 78637, 154, 1878, 8,
+'Nordeste', 'SE', 28, '30/8/2020', 36, 2298696, 72417, 137, 1844, 5,
+'Nordeste', 'BA', 29, '30/8/2020', 36, 14873064, 256062, 1272, 5344, 42,
+'Sudeste', 'MG', 31, '30/8/2020', 36, 21168791, 215050, 2485, 5326, 56,
+'Sudeste', 'ES', 32, '30/8/2020', 36, 4018650, 110351, 326, 3135, 6,
+'Sudeste', 'RJ', 33, '30/8/2020', 36, 17264943, 223302, 345, 16027, 11,
+'Sudeste', 'SP', 35, '30/8/2020', 36, 45919049, 803404, 1982, 29978, 34,
+'Sul', 'PR', 41, '30/8/2020', 36, 11433957, 130603, 1636, 3251, 17,
+'Sul', 'SC', 42, '30/8/2020', 36, 7164788, 146864, 460, 2235, 17,
+'Sul', 'RS', 43, '30/8/2020', 36, 11377239, 124482, 66, 3395, 10,
+'Centro-Oeste', 'MS', 50, '30/8/2020', 36, 2778986, 48522, 499, 851, 11,
+'Centro-Oeste', 'MT', 51, '30/8/2020', 36, 3484466, 90337, 375, 2725, 19,
+'Centro-Oeste', 'GO', 52, '30/8/2020', 36, 7018354, 130863, 547, 3075, 15,
+'Centro-Oeste', 'DF', 53, '30/8/2020', 36, 3015268, 160796, 1270, 2469, 19,
+'Norte', 'RO', 'Porto Velho', 11, 110020, 11004, 'MADEIRA-MAMORE', '30/8/2020', 36, 529544, 26744, 96, 642, 0, 1,
+'Norte', 'AC', 'Rio Branco', 12, 120040, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 407319, 9983, 9, 383, 1, 1,
+'Norte', 'AM', 'Manaus', 13, 130260, 13001, 'MANAUS,  ENTORNO E ALTO RIO NEGRO', '30/8/2020', 36, 2182763, 42398, 52, 2246, 2, 1,
+'Nordeste', 'CE', 'Fortaleza', 23, 230440, 23001, '1ª REGIAO FORTALEZA', '30/8/2020', 36, 2669342, 46689, 58, 3778, 1, 1,
+'Nordeste', 'PE', 'Recife', 26, 261160, 26010, 'RECIFE', '30/8/2020', 36, 1645727, 31770, 128, 2283, 1, 1,
+'Nordeste', 'BA', 'Salvador', 29, 292740, 29020, 'SALVADOR', '30/8/2020', 36, 2872347, 76373, 111, 2419, 4, 1,
+'Sudeste', 'MG', 'Belo Horizonte', 31, 310620, 31016, 'BELO HORIZONTE/ NOVA LIMA/ CAETE', '30/8/2020', 36, 2512070, 33092, 0, 978, 10, 1,
+'Sudeste', 'RJ', 'Rio de Janeiro', 33, 330455, 33005, 'METROPOLITANA I', '30/8/2020', 36, 6718903, 90345, 82, 9634, 6, 1,
+'Sudeste', 'SP', 'São Paulo', 35, 355030, 35016, 'SAO PAULO', '30/8/2020', 36, 12252023, 257778, 250, 11400, 2, 1,
+'Sul', 'PR', 'Curitiba', 41, 410690, 41002, '2ª RS METROPOLITANA', '30/8/2020', 36, 1933105, 28647, 252, 976, 8, 1,
+'Centro-Oeste', 'DF', 'Brasília', 53, 530010, 53001, 'DISTRITO FEDERAL', '30/8/2020', 36, 3015268, 160796, 1270, 2469, 19, 1)
+
+
+#Exercício letra b)
+
+print(Estados[42])
+
+#Lista
+print('O número de casos acumulados no Rio de Janeiro são: ', Estados[186])
+#Tupla
+print('O número de casos acumulados no Rio de Janeiro são: ', EstadosT[186])
+
+#Exercício letra c)
+
+obitosT = [Estados[8]+Estados[18]+Estados[28]+Estados[38]+Estados[48]+Estados[58]+Estados[68]+Estados[78]+Estados[88]+Estados[98]+Estados[108]+Estados[118]+Estados[128]+Estados[138]+Estados[148]+Estados[158]+Estados[168]+Estados[178]+Estados[188]+Estados[198]+Estados[208]+Estados[218]+Estados[228]+Estados[238]+Estados[248]+Estados[258]+Estados[268]+Estados[278]]
+print('O número de óbitos total é: ', sum(obitosT))
+
+#Exercício letra d)
+
+#Lista
+print('Número de óbitos da Paraíba: ', Estados[119])
+obitoCorretoPB = int(input('Entre com o número de óbitos corretos da Paraíba: '))
+Estados[119] = obitoCorretoPB
+print('Números de óbitos da Paraíba atualizados com sucesso! ', Estados[119])
+
+#Tupla - Não é possível realizar esse tipo de operação em Tuplas.
+'''
+print('Número de óbitos da Paraíba: ', EstadosT[119])
+obitoCorretoPB = int(input('Entre com o número de óbitos corretos da Paraíba. '))
+EstadosT[119] = obitoCorretoPB
+print('Números de óbitos da Paraíba atualizados com sucesso! ', EstadosT[119])
+'''
+
+#Exercício letra e)
+
+Acre = ['Norte', 'AC', 'Acrelândia', 12, 120001, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 15256, 428, 0, 8, 0, 0,
+'Norte', 'AC', 'Assis Brasil', 12, 120005, 12001, 'ALTO ACRE', '30/8/2020', 36, 7417, 503, 0, 9, 0, 0,
+'Norte', 'AC', 'Brasiléia', 12, 120010, 12001, 'ALTO ACRE', '30/8/2020', 36, 26278, 1113, 4, 17, 0, 0,
+'Norte', 'AC', 'Bujari', 12, 120013, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 10266, 366, 0, 6, 0, 0,
+'Norte', 'AC', 'Capixaba', 12, 120017, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 11733, 246, 0, 7,0, 0,
+'Norte', 'AC', 'Cruzeiro do Sul', 12, 120020, 12003, 'JURUA E TARAUACA/ENVIRA', '30/8/2020', 36, 88376, 3132, 0, 54, 0, 0,
+'Norte', 'AC', 'Epitaciolândia', 12, 120025, 12001, 'ALTO ACRE', '30/8/2020', 36, 18411, 487, 0, 13, 0, 0,
+'Norte', 'AC', 'Feijó', 12, 120030, 12003, 'JURUA E TARAUACA/ENVIRA', '30/8/2020', 36, 34780, 1114, 23, 18, 0, 0,
+'Norte', 'AC', 'Jordão', 12, 120032, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 8317, 155, 0, 1, 0, 0,
+'Norte', 'AC', 'Mâncio Lima', 12, 120033, 12003, 'JURUA E TARAUACA/ENVIRA', '30/8/2020', 36, 18977, 665, 0, 9, 0, 0,
+'Norte', 'AC', 'Manoel Urbano', 12, 120034, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 9459, 269, 0, 2, 0, 0,
+'Norte', 'AC', 'Marechal Thaumaturgo', 12, 120035, 12003, 'JURUA E TARAUACA/ENVIRA', '30/8/2020', 36, 18867, 335, 0, 1, 0, 0,
+'Norte', 'AC', 'Plácido de Castro', 12, 120038, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 19761, 397, 0, 8, 0, 0,
+'Norte', 'AC', 'Porto Walter', 12, 120039, 12003, 'JURUA E TARAUACA/ENVIRA', '30/8/2020', 36, 11982, 258, 0, 2, 0, 0,
+'Norte', 'AC', 'Rio Branco', 12, 120040, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 407319, 9983, 9, 383, 1, 1,
+'Norte', 'AC', 'Rodrigues Alves', 12, 120042, 12003, 'JURUA E TARAUACA/ENVIRA', '30/8/2020', 36, 18930, 142, 0, 6, 0, 0,
+'Norte', 'AC', 'Santa Rosa do Purus', 12, 120043, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 6540, 274, 0, 2, 0, 0,
+'Norte', 'AC', 'Senador Guiomard', 12, 120045, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 23024, 464, 0, 11, 0, 0,
+'Norte', 'AC', 'Sena Madureira', 12, 120050, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 45848, 1401, 0, 11, 0, 0,
+'Norte', 'AC', 'Tarauacá', 12, 120060, 12003, 'JURUA E TARAUACA/ENVIRA', '30/8/2020', 36, 42567, 1495, 0, 14, 0, 0,
+'Norte', 'AC', 'Xapuri', 12, 120070, 12001, 'ALTO ACRE', '30/8/2020', 36, 19323, 917, 0, 12, 0, 0,
+'Norte','AC', 'Porto Acre', 12, 120080, 12002, 'BAIXO ACRE E PURUS', '30/8/2020', 36, 18504, 494, 1, 15, 0]
+
+Estados.append(Acre)
+
+#Exercício letra f)
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('BAIXO ACRE E PURUS')
+Acre.remove('ALTO ACRE')
+Acre.remove('ALTO ACRE')
+Acre.remove('ALTO ACRE')
+Acre.remove('ALTO ACRE')
+Acre.remove('JURUA E TARAUACA/ENVIRA')
+Acre.remove('JURUA E TARAUACA/ENVIRA')
+Acre.remove('JURUA E TARAUACA/ENVIRA')
+Acre.remove('JURUA E TARAUACA/ENVIRA')
+Acre.remove('JURUA E TARAUACA/ENVIRA')
+Acre.remove('JURUA E TARAUACA/ENVIRA')
+Acre.remove('JURUA E TARAUACA/ENVIRA')
+
+#Exercício letra g
+populacaoAC = 881935
+casosAcumladosAC = 22933
+casosNovosAC = 328
+obitosAcumuladosAC = 590
+obitosNovosAC = 8
+
+populacaoACS = (Acre[8]+Acre[22]+Acre[36]+Acre[50]+Acre[64]+Acre[78]+Acre[92]+Acre[106]+Acre[120]+Acre[134]+Acre[148]+Acre[162]+Acre[176]+Acre[190]+Acre[204]+Acre[218]+Acre[232]+Acre[246]+Acre[260]+Acre[274]+Acre[288]+Acre[302])
+casosAcumladosACS = (Acre[9]+Acre[23]+Acre[37]+Acre[51]+Acre[65]+Acre[79]+Acre[93]+Acre[107]+Acre[121]+Acre[135]+Acre[149]+Acre[163]+Acre[177]+Acre[191]+Acre[205]+Acre[219]+Acre[233]+Acre[247]+Acre[261]+Acre[275]+Acre[289]+Acre[303])
+casosNovosACS = (Acre[10]+Acre[24]+Acre[38]+Acre[52]+Acre[66]+Acre[80]+Acre[94]+Acre[108]+Acre[122]+Acre[136]+Acre[150]+Acre[164]+Acre[178]+Acre[192]+Acre[206]+Acre[220]+Acre[234]+Acre[248]+Acre[262]+Acre[276]+Acre[290]+Acre[304])
+obitosAcumuladosACS = (Acre[11]+Acre[25]+Acre[39]+Acre[53]+Acre[67]+Acre[81]+Acre[95]+Acre[109]+Acre[123]+Acre[137]+Acre[151]+Acre[165]+Acre[179]+Acre[193]+Acre[207]+Acre[221]+Acre[235]+Acre[249]+Acre[263]+Acre[277]+Acre[291]+Acre[305])
+obitosNovosACS = (Acre[12]+Acre[26]+Acre[40]+Acre[54]+Acre[68]+Acre[82]+Acre[96]+Acre[110]+Acre[124]+Acre[138]+Acre[152]+Acre[166]+Acre[180]+Acre[194]+Acre[208]+Acre[222]+Acre[236]+Acre[250]+Acre[264]+Acre[278]+Acre[292]+Acre[306])
+
+var1 = populacaoAC != populacaoACS
+var2 = casosAcumladosAC != casosAcumladosACS
+var3 = casosNovosAC != casosNovosACS
+var4 = obitosNovosAC != obitosNovosACS
+var5 = obitosAcumuladosAC != obitosAcumuladosACS
+
+print('A população foi alterada? ', str(var1))
+print('O número de casos acumulados foi alterado? ', str(var2))
+print('O número de casos novos foi alterado? ', str(var3))
+print('O numero de óbitos acumulados foi alterado? ', str(var5))
+print('A número de óbitos novos foi alterado? ', str(var4))
+
+#Exercício letra h
+Acre.insert(6,'BAIXO ACRE E PURUS')
+Acre.insert(21,'ALTO ACRE')
+Acre.insert(36,'ALTO ACRE')
+Acre.insert(51,'BAIXO ACRE E PURUS')
+Acre.insert(66,'BAIXO ACRE E PURUS')
+Acre.insert(81,'JURUA E TARAUACA/ENVIRA')
+Acre.insert(96,'ALTO ACRE')
+Acre.insert(111,'JURUA E TARAUACA/ENVIRA')
+Acre.insert(126,'BAIXO ACRE E PURUS')
+Acre.insert(141,'JURUA E TARAUACA/ENVIRA')
+Acre.insert(156,'BAIXO ACRE E PURUS')
+Acre.insert(171,'JURUA E TARAUACA/ENVIRA')
+Acre.insert(186,'BAIXO ACRE E PURUS')
+Acre.insert(201,'JURUA E TARAUACA/ENVIRA')
+Acre.insert(216,'BAIXO ACRE E PURUS')
+Acre.insert(231,'JURUA E TARAUACA/ENVIRA')
+Acre.insert(246,'BAIXO ACRE E PURUS')
+Acre.insert(261,'BAIXO ACRE E PURUS')
+Acre.insert(276,'BAIXO ACRE E PURUS')
+Acre.insert(291,'JURUA E TARAUACA/ENVIRA')
+Acre.insert(306,'ALTO ACRE')
+Acre.insert(321,'BAIXO ACRE E PURUS')
+
+#Exercício letra i
+
+print('O número mínimo de óbitos novos no estado do Acre é: ', min(Acre[13], Acre[28], Acre[43], Acre[58], Acre[73], Acre[88], Acre[103], Acre[118], Acre[133], Acre[148], Acre[163], Acre[178], Acre[193], Acre[208], Acre[223], Acre[238], Acre[253], Acre[268], Acre[283], Acre[298], Acre[313], Acre[328]))
+print('O número máximo de óbitos novos no estado do Acre é: ', max(Acre[13], Acre[28], Acre[43], Acre[58], Acre[73], Acre[88], Acre[103], Acre[118], Acre[133], Acre[148], Acre[163], Acre[178], Acre[193], Acre[208], Acre[223], Acre[238], Acre[253], Acre[268], Acre[283], Acre[298], Acre[313], Acre[328]))
+
+#Exercício letra j
+#Não consegui fazer, professor.
