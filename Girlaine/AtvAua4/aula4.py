b21bd9ab43c75c065d497313f7ed0006c544bad4
Girlaine-ISD <girlaine.melo@edu.isd.org.br>
2020-09-15 01:27:06 -0300

    AtivAula4

---
+++ Girlaine/AtvAua4/aula4.py
@@ -0,0 +1,132 @@
+logicValue = int(input('você é uma pessoa inteligetne? (responda 0 para não e 1 para sim)'))
+
+if(logicValue):
+    print('não deixe o trabalho para depois !!!')
+    print('não deixe o trabalho para depois !!!')
+    print('não deixe o trabalho para depois !!!')
+    print('não deixe o trabalho para depois !!!')
+    print('não deixe o trabalho para depois !!!')
+    print('não deixe o trabalho para depois !!!')
+
+print('depois do If')
+
+botaoApertado = int(input("o rato apertou o botão 1 ? (responda 0 para não e 1 para sim)"))
+botaoApertado = int(input("o rato apertou o botão ? (responda 0 para não e 1 para sim"))
+
+#operadores lógicos not,an,or
+
+not
+0 -> 1
+1 -> 0
+and 
+b1 b2 R
+0 0 0
+0 1 0
+1 0 0
+1 1 1
+ord 
+0 0 0
+0 1 1
+1 0 1
+1 1 1 
+
+botao1Apertado = int(input("o rato apertou o botão 1 ? (responda 0 para não e 1 para sim)"))
+botao2Apertado = int(input("o rato apertou o botão ? (responda 0 para não e 1 para sim"))
+
+if(botao1Apertado):
+    print('o botao 1 foi apertado )
+if(not botao1Apertado):
+    print('botao 1 não foi apertado')
+if(botao2Apertado):
+    print('os dois botões foram apertados')
+if(botao1Apertado and botao2Apertado):
+    print('os dois botãoes foram apertados')
+    if(botao1Apertado and not botao2Apertado):
+    print('os dois botãoes não foram apertados'):
+if(botao1Apertado and not botao2Apertado):
+    print('os dois botão  não foram apertados')
+
+
+if(not botao1Apertado or not botao2Apertado):
+    print('os dois botãoes não foram apertados')
+if(not botao1Apertado or botao2Apertado):
+    print('o botão 1 não foi apertado ou o botão 2 foi apertado'):
+if(botao1Apertado or not botao2Apertado):
+    print(' o botãoo 1 foi apertado  eo botão 2 não foi apertado')
+ if(botao1Apertado or botao2Apertado):
+    print('os dois botões foram aprtados')
+
+botaoApertado = int(input("o rato apertou o botão 1 ? (responda n para não e s para sim)"))
+botaoApertado = int(input("o rato apertou o botão ? (responda n para não e s para sim"))
+
+if(botao1Apertado == 's'):
+    print(' o botão 1 foi apertado')
+if(botao1Apertado == 'n'):
+    print('o botão 1 não foi apertado')
+if(botao2Apertado == 's'):
+    print(' o botã 2 foi apertado')
+if(botao2Apertado == 'n'):
+    print('o botão 2 não foi apertado')
+
+if(not(botao2Apertado == 's')):
+    print('o botão 2 não foi apertado')
+if(not(botao2Apertado [= 's')):
+    print(' o botão 2 não foi apertado')
+if(not botao1Apertado an not botao2Apertado):
+    print('os dois botões não foram apertados')
+if(not botao1Apertado and botao2Apertado):
+    print(' somente obotão 1 foi apertado ')
+
+
+botao1Apertado = int(input("o rato apertou o botão 1 ? (responda 0 para não e 1 para sim)"))
+
+if(botaoApertado):
+    print(' o btão foi apertado')
+    print(' o btão foi apertado')
+    print(' o btão foi apertado')
+    print(' o btão foi apertado')
+
+else:
+    print('o botão não foi apertado')
+    print('o botão não foi apertado')
+    print('o botão não foi apertado')
+    print('o botão não foi apertado')
+    print('o botão não foi apertado')
+    print('o botão não foi apertado')
+    print('o botão não foi apertado')
+    if(True):
+        print('true')
+
+print('depois do if')
+
+numero digitado = int(input('digite um numero'))
+if(numeroDigitado > 5):
+    print(' o numero é maior que 5 \n')
+else:
+    print('o numero é menor ou iguala 5 \n')
+
+
+
+numero digitado = int(input('digite um numero'))
+
+if(numeroDigitado < 5):
+    print(' o numero é menor que 5 \n')
+elif(numeroDigitado < 10 ):
+    print('o numero é menor que 10 \n')
+elif (numeroDigitado < 15 ):
+    print('o numero é menor que 15 \n')
+elif (numeroDigitado < 20 ):
+    print('o numero é menor que 20')
+else:
+    print('o numero é menor ou iguala 20 \n')
+
+
+
+if(not botao1Apertado or not botao2Apertado):
+    print('os dois botãoes não foram apertados')
+elif (not botao1Apertado or botao2Apertado):
+    print('o botão 1 não foi apertado ou o botão 2 foi apertado'):
+elif(botao1Apertado or not botao2Apertado):
+    print(' o botãoo 1 foi apertado  eo botão 2 não foi apertado')
+ else(botao1Apertado or botao2Apertado):
+    print('os dois botões foram aprtados')
