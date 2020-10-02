042f6dd9046062c38e325c236c396608c5791187
jonathabsilva <jonathab.silva@gmail.com>
2020-10-02 15:30:24 -0300

    Atualização 02/10/2020

--- Jonatha/Aula 08/Atividade Contextualizada/atividadeContextualizada08.py
+++ Jonatha/Aula 08/Atividade Contextualizada/atividadeContextualizada08.py
@@ -5,8 +5,8 @@ import math
 sensor1 = list()
 sensor2 = list()
 variaveis = ('ax','ay','az','gx','gy','gz')
-angulo = list()
-
+angulo1 = list()
+angulo2 = list()
 
 def ang(sensor, angulo, M=0.98, dt=0.05):
   dadoa = (sensor['ax']/(sensor['ay']**2 + sensor['az']**2)*180/math.pi)
@@ -19,25 +19,37 @@ def ang(sensor, angulo, M=0.98, dt=0.05):
 
 with open('coletaFlexJoelho.csv', 'r', newline='') as arquivo:
   spamreader = csv.reader(arquivo, delimiter="'", quotechar='|')
-
   j = 0
   for linha in spamreader:
-    saida = linha[0].split(',""')[0].split('"')[1].split('[')[1].split(']')[0].split(',')
+    saida1 = linha[0].split(',""')[0].split('"')[1].split('[')[1].split(']')[0].split(',')
+    saida2 = linha[0].split(',""')[1].split('""')[0].split('[')[1].split(']')[0].split(',')
     sensor1.append(dict())
-    for i in range(len(saida)):
-      sensor1[j][variaveis[i]] = float(saida[i])
+    sensor2.append(dict())
+    for i in range(len(saida1)):
+      sensor1[j][variaveis[i]] = float(saida1[i])
+      sensor2[j][variaveis[i]] = float(saida2[i])
     j+=1
 
 arquivoTxt = open('anguloProcessado.txt','w')
 arquivoCsv = open('anguloProcessado.csv', 'w', newline='')
 spamwriter = csv.writer(arquivoCsv, delimiter = ' ', quotechar = '|', quoting=csv.QUOTE_MINIMAL)
 
-arquivoTxt.write('Sensor 1:\n')
+arquivoTxt.write('\n\nSensor 1:\n')
 
 for x in sensor1:
-  angulo.append(ang(x,angulo))
+  angulo1.append(ang(x,angulo1))
 
-for i in angulo:
+for i in angulo1:
+  print(i)
+  arquivoTxt.write(str(i))
+  spamwriter.writerow(str(i))
+
+for x in sensor2:
+  angulo2.append(ang(x, angulo2))
+
+
+arquivoTxt.write('\n\nSensor 2:\n')
+for i in angulo2:
   print(i)
   arquivoTxt.write(str(i))
   spamwriter.writerow(str(i))
@@ -45,11 +57,3 @@ for i in angulo:
 print('Saidas geradas')
 arquivoTxt.close()
 arquivoCsv.close()
-
-
-
-
-
-
-
-
